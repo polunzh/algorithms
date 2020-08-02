b870821422cea02320a5ee5691dd4a73249ca8b4
polunzh <polunzh@gmail.com>
2020-08-02 21:32:52 +0800

    add bst bree

---
+++ js/tree/bst.js
@@ -0,0 +1,60 @@
+const Node = require('./node');
+
+class BST {
+  constructor(node = null) {
+    this.root = node;
+  }
+
+  insert(data) {
+    const node = new Node(null, null, data);
+    if (this.root === null) {
+      this.root = node;
+      return;
+    }
+
+    let current = this.root;
+    let parent = null;
+    while (true) {
+      parent = current;
+      if (node.data < current.data) {
+        current = current.left;
+        if (current === null) {
+          parent.left = node;
+          break;
+        }
+      } else {
+        current = current.right;
+        if (current === null) {
+          parent.right = node;
+          return;
+        }
+      }
+    }
+  }
+
+  preOrderTraversal(node) {
+    if (node !== null) {
+      console.log(node.data);
+      this.preOrderTraversal(node.left);
+      this.preOrderTraversal(node.right);
+    }
+  }
+
+  inOrderTraversal(node) {
+    if (node !== null) {
+      this.inOrderTraversal(node.left);
+      console.log(node.data);
+      this.inOrderTraversal(node.right);
+    }
+  }
+
+  postOrderTraversal(node) {
+    if (node !== null) {
+      this.postOrderTraversal(node.left);
+      this.postOrderTraversal(node.right);
+      console.log(node.data);
+    }
+  }
+}
+
+module.exports = BST;
---
+++ js/tree/node.js
@@ -0,0 +1,9 @@
+class Node {
+  constructor(left, right, data) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+module.exports = Node;
